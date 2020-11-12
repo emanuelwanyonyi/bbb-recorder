ac4f3978d2691d41a64393baefd760370ca4d104
Emanuel Dickson Wanyonyi <ewanyonyi@kenet.or.ke>
2020-11-12 10:48:35 +0300

    Updated environment variables in 'start_export.sh' file

--- start_export.sh
+++ start_export.sh
@@ -2,6 +2,8 @@
 # Script to launch a live rtmp streaming session
 # The script populates .env file with environment variables passed by docker-compose.yml
 
+echo ffmpegServer=$FFMPEG_SERVER >> .env
+echo ffmpegServerPort=$FFMPEG_SERVER_PORT >> .env
 echo auth=$AUTH >> .env
 echo copyToPath=$COPY_TO_PATH >> .env
 echo playbackFile=$PLAYBACK_FILE >> .env
